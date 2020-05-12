e0185c4d85facbd022011d37de455835260d9648
thisisakshi <akshiagarwal@gmail.com>
2020-05-12 00:33:45 -0400

    box size fixed again

--- js/coronavirusMap.js
+++ js/coronavirusMap.js
@@ -3,7 +3,7 @@ function coronavirus() {
   d3.selectAll("svg").remove();
   // The svg
   var svg = d3.select(".tableRight")
-    .append("svg").attr("width", 800).attr("height", 622),
+    .append("svg").attr("width", 800).attr("height", 600),
     width = +svg.attr("width"),
     height = +svg.attr("height");
   
